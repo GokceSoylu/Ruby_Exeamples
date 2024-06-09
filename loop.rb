e508041a61487c940bdf5f341cd7eaa2752bdfe5
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-06-09 03:11:05 +0300

    varable begin

---
+++ loop.rb
@@ -0,0 +1,12 @@
+#ruby ile alakalı ilk notlar
+#varaible tanımı python gibi direkt adını yazıp tanımlarız.
+a=1
+b="hasan"
+c="sabbah"
+puts a*90 #zaten gördün yazdır sırasında işlem yaptırısın ama tabiki kalıcı olmaz
+
+#ruby değişken tipleri deediğimiz zaman üç tipten bahsederzi
+#global varaible $name
+#instance varaible @instance
+#class name @@name
+#constan varaible NAME
