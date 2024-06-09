9c5c2e840c111618e4faf5795306cbe1175b0b78
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-06-09 10:01:02 +0300

    gets p chomp

---
+++ strings2.rb
@@ -0,0 +1,11 @@
+# == for string exacly same so David != david
+str = gets
+puts str
+
+#p method hocam bu arkadaş stringin içinde tam olarak ne olduğunu bize karakter karakter gösterir. mesela gets kullandığımızda ek olrak \n karakteride eklenir vb.
+p str
+
+#chomp metodu sayesinde de gets \n karakteri olmaksızın string alır
+str2=gets.chomp
+p str2
+
