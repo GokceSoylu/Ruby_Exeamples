d72211aa508ce6fe78ad52861e2ee3324187d9e8
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-06-11 23:06:03 +0300

    spaceship oparator

---
+++ operators.rb
@@ -0,0 +1,11 @@
+#<=> greater equal or less 
+#hocam arkadaşın adı spaceship operator iki  arkadaşı kıyaslar 
+# a<=> b
+# a<b -1
+# a=b 0
+# a>b 1 döndürür
+a=10
+b=11
+puts a<=>b
+puts (a+1)<=>b
+puts (a+2)<=>b
